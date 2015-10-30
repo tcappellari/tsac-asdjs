378cb46a323e7e2d7da4e2da841f75765d70c7d5
Tiziano Cappellari <tcappellari@nabertech.com>
2015-10-30 13:32:42 +0100

    ex_1 and ex_2

---
+++ homework_01.js
@@ -0,0 +1,76 @@
+/*
+Esercizio 1
+Dato un array di interi, restituire la loro somma fino a che non viene ritrovato un valore negativo
+*/
+// Iterative
+// Spazio: O(1) 
+// Tempo: O(n)
+function sumwhileneg(myarray) {
+    var tot = 0;
+    for(i = 0; i < myarray.length; ++i) {
+        var x = myarray[i];
+        if (x > 0) {
+            tot += x;
+        } else {
+            return tot;
+        }
+    }
+    return tot;
+}
+
+
+// Ricorsive
+// Spazio: O(n) 
+// Tempo: O(n)
+function sumwhilenegR(myarray) {
+    if (myarray.length == 0 || myarray[0] < 0) {
+        return 0;
+    } else {
+        return myarray[0] + sumwhilenegR(myarray.slice(1));
+    }
+}
+
+// Per Tiziano.
+function ex_1_I(myarray) {
+    sumwhileneg(myarray);
+}
+
+function ex_1_R(myarray) {
+    sumwhilenegR(myarray);
+}
+
+/*
+Esercizio 2
+Dato un numero n, restituire la somma dei primi n numeri interi positivi dispari
+*/
+// Spazio: O(1) 
+// Tempo: O(n)
+function firstOdd(n) {
+    var tot = 0;
+    for(i = 0; i < n; ++i) {
+        tot += 1 + 2 * i;
+    }
+    return tot;
+}
+
+
+// Spazio: O(n) 
+// Tempo: O(n)
+function firstOddR(n) {
+    if (n == 0) {
+        return 0;
+    } else {
+        return 2 * n - 1 + firstOddR(n - 1);
+    }
+}
+
+
+
+// Per Tiziano.
+function ex_2_I(x) {
+    sumwhileneg(x);
+}
+
+function ex_2_R(x) {
+    sumwhilenegR(x);
+}
